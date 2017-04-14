6bc28cdf166ffd8a05946f2da4bd88e7d8b56030
Merve Başak <merve.basak15@gmail.com>
2017-04-15 01:12:11 +0200

    çarpım tablosu

---
+++ src/Challenge1.java
@@ -0,0 +1,26 @@
+public class Challenge1 {
+    public static void main(String[] args) {
+        // System.out.println("1*1=1");
+        // System.out.println("1*2=2");
+        // System.out.println("1*3=3");
+
+
+        /*k = k+ 1;
+        System.out.println(i + "*" + k + "=" + i * k);
+        k = k + 1;
+        System.out.println(i + "*" + k + "=" + i * k);
+        k = k + 1;
+        System.out.println(i + "*" + k + "=" + i * k);*/
+        for (int n = 1; n <= 10; n++) {
+            for (int m = 1; m <= 10; m++) {
+                System.out.println(n + "*" + m + "=" + n * m);
+            }
+        }
+
+
+    }
+
+}
+
+
+
