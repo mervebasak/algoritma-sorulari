072c76847a316bf476f16d86aecc26fe276e489a
Merve Başak <merve.basak15@gmail.com>
2017-04-23 15:58:01 +0200

    yıldız yazdırma

---
+++ src/challenge2.java
@@ -0,0 +1,21 @@
+public class challenge2 {
+    public static void main(String[] args) {
+      /*  System.out.println("*");( çok düz ilk aklıma gelen yapıyım dedim yinede)
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****"); */
+
+        for (int i = 1; i <5 ; i++) {
+            for (int j = 0; j <i ; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+         }
+        }
+    }
+
+
+
+
+
+
