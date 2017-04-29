8a945e026005b85cce84372051ae77bbd9ce3660
Merve Başak <merve.basak15@gmail.com>
2017-04-29 13:29:40 +0300

    kelime sayısı

---
+++ src/Challenge4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+   public class Challenge4 {
+        public static void main(String[] args){
+            int k=0;
+            System.out.println("Bir Cümle Giriniz=>");
+            Scanner a=new Scanner(System.in);
+            String sentence=a.nextLine();
+
+            for(int i=0;i<sentence.length();i++){ //ne kadar döneceğini bilmek için var
+                if(sentence.charAt(i)==' ')
+                    k++; //k önce boşlukları saymak için
+            }
+            System.out.println("Girilen Cümledeki Kelime Sayısı="+(k+1));//k+1 kelime sayısı
+        }
+
+    }
+
+
+
+
+
+
